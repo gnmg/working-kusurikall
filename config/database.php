76e3aa21c06964fb7380ea5da454f434e9d9f0f7
gnmg <ralph@ralphreene.info>
2021-05-26 08:44:07 +0900

    fix rds db name

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOSTNAME', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DB_NAME', 'forge'),
+            'database' => env('DB_NAME', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
