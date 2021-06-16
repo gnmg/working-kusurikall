8b9f9ec596ba556a54c31bb9eb62fec38ba5f63e
gnmg <ralph@ralphreene.info>
2021-06-16 15:52:26 +0900

    delete seconds from time

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -58,7 +58,7 @@
                                         {{ $call -> title }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        {{ $call -> time }}
+                                        {{str_replace(':00', '', $call -> time)}}
                                     </td>
                                     <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                         0{{ $call -> phone_numbers }}
