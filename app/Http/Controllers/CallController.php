ab4789d03d5bba6fcd701f13ab2b06ca1df13776
gnmg <ralph@ralphreene.info>
2021-08-18 17:39:46 +0900

    change regex

--- app/Http/Controllers/CallController.php
+++ app/Http/Controllers/CallController.php
@@ -47,7 +47,7 @@ class CallController extends Controller
         $validatedData = $request->validate([
             'title' => 'required|max:50',
             'time' => 'required',
-            'phone_numbers' => ['required', 'regex:/^0([0-9][0-9]{4}[0-9]{2}[0-9]{3}|[0-9]{3}[0-9]{2}|[0-9]{4}[0-9])[0-9]{4}$/'],
+            'phone_numbers' => ['required', 'regex:/^(0([0-9][0-9]{4}[0-9]{2}[0-9]{3}|[0-9]{3}[0-9]{2}|[0-9]{4}[0-9])[0-9]{4}$|050-?\d{4}-?\d{4})$/'],
             // 'phone_numbers' => 'required',
             'message_voice' => 'required',
             'message' => 'required',
