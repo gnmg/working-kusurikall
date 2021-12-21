4ff52d7734e32248ddc4226aae4c05f4368b2f4c
gnmg <ralph@ralphreene.info>
2021-12-21 11:06:16 +0900

    change text

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,8 +14,7 @@
             @csrf
             <div class="mb-2 text-3xl text-center">無料お試し申し込み</div>
             <div class="text-base mb-7"><span class="font-bold">
-                    14日間 無料で利用できる
-                </span> メールワイズアカウントを作成できます。無料期間の終了後、自動的に課金されることはありません。</div>
+                </span> 登録して、14日間の無料体験を開始しましょう。クーポンコードをお持ちの方は、決済画面に入力すると、さらに追加16日間、合計30日間の無料体験をご利用いただけます。体験期間が終了するまで、クレジットカードには課金されません。</div>
             <!-- Name -->
             <div>
                 <x-label for="name" />{{ __('lang.name')}}
@@ -48,14 +47,16 @@
                     name="password_confirmation" required />
             </div>
 
-             <!-- Telephone -->
+            <!-- Telephone -->
             <div class="mt-4">
                 <x-label for="user_telephone" />{{ __('lang.user_telephone')}}
 
-                <x-input id="user_telephone" class="block w-full mt-1" type="text" placeholder="{{ __('lang.user_telephone_placeholder')}}" name="user_telephone"  required
-                    autofocus />
+                <x-input id="user_telephone" class="block w-full mt-1" type="text"
+                    placeholder="{{ __('lang.user_telephone_placeholder')}}" name="user_telephone" required autofocus />
             </div>
-            <div class="pt-3"><input class="mr-1" type="checkbox" name="checkbox" value="check" id="agree" /><a href="{{ url('/terms') }}" class="ml-2 text-indigo-600">利用規約</a>・<a href="{{ url('/privacy') }}" target="_blank" class="text-indigo-600">プライバシーポリシー</a>に同意の上でお進みください。</div>
+            <div class="pt-3"><input class="mr-1" type="checkbox" name="checkbox" value="check" id="agree" /><a
+                    href="{{ url('/terms') }}" class="ml-2 text-indigo-600">利用規約</a>・<a href="{{ url('/privacy') }}"
+                    target="_blank" class="text-indigo-600">プライバシーポリシー</a>に同意の上でお進みください。</div>
 
 
             <div class="flex items-center justify-end mt-4">
