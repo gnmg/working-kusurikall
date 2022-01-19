14b3f6599a0ac8b455dceb3787d7b110ecc96fe0
gnmg <ralph@ralphreene.info>
2022-01-19 15:40:27 +0900

    add leadydyno spark app

--- resources/views/vendor/spark/app.blade.php
+++ resources/views/vendor/spark/app.blade.php
@@ -13,7 +13,12 @@
     <script src="https://js.stripe.com/v3/"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js" integrity="sha512-rmZcZsyhe0/MAjquhTgiUcb4d9knaFc7b5xAfju483gbEXTkeJRUMIPk6s3ySZMYUHEcjKbjLjyddGWMrNEvZg==" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js" integrity="sha512-90vH1Z83AJY9DmlWa8WkjkV79yfS2n2Oxhsi2dZbIv0nC4E6m5AbH8Nh156kkM7JePmqD6tcZsfad1ueoaovww==" crossorigin="anonymous"></script>
-
+    <script type="text/javascript" src="https://static.leaddyno.com/js"></script>
+    <script>
+    LeadDyno.key = "a7d557dba0387e907b3f5359b186b2d26eb294cd";
+    LeadDyno.recordVisit();
+    LeadDyno.autoWatch();
+    </script>
     <!-- Styles -->
     <style>
         {!! file_get_contents($cssPath) !!}
